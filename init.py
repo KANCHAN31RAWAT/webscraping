4c7625c2666fca6f2a8c9aa68ac164e7098fd8f9
Priya-Pathak <priyapathak24.07@gmail.com>
2021-02-06 16:33:51 +0530

    Adding flask

--- init.py
+++ init.py
@@ -1,3 +1,16 @@
-import core
+# from flask import Flask
+from flask import Flask, render_template 
 
-core.main()
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+    
+    
+# @app.route("/salvador")
+# def salvador():
+#     return "Hello, Salvador"
+    
+if __name__ == "__main__":
+    app.run(debug=True)
